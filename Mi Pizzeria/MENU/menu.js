ef5fee8facfe78d490709cf35c29f8348133c49d
ChaponanWill <chapowilliam09@gmail.com>
2026-01-15 20:33:37 -0500

    añadiendo menú

---
+++ Mi Pizzeria/MENU/menu.js
@@ -0,0 +1,104 @@
+export const menu = [
+    {
+        id: 1,
+        name: "Clásicas",
+        image: "https://techakim.com/sam/tg/7268/li/imgs/pizza.jpg",
+        pizzas_disponibles: [
+            {
+                id: 1,
+                name: "Americana",
+                image: "https://i.pinimg.com/736x/cf/da/04/cfda0486329b8eb3976c25e000427cc6.jpg",
+                description: "Jamón y Hot Dog",
+                price: 27,
+                bandera_image:
+                    "https://st2.depositphotos.com/1000487/10460/v/950/depositphotos_104607398-stock-illustration-united-states-flag.jpg",
+            },
+            {
+                id: 2,
+                name: "Hawaiana",
+                image: "https://i.pinimg.com/736x/01/18/72/011872639c2590044e954ddcd7b1e254.jpg",
+                description: "Jamón y Piña Golden en Almibar",
+                price: 32,
+                bandera_image:
+                    "https://media.istockphoto.com/id/530810572/es/vector/brasil-bandera-de-ilustraci%C3%B3n-vectorial.jpg?s=612x612&w=0&k=20&c=hu4BPeiDzfttyfE_NrNJuqoQmNIF04HTsaaV81_o88g=",
+            },
+            {
+                id: 3,
+                name: "Peperoni",
+                image: "https://i.pinimg.com/736x/fc/aa/79/fcaa7948e06992862f2b3bd480ba4ae9.jpg",
+                description: "Full Peperoni",
+                price: 32,
+                bandera_image:
+                    "https://imagedelivery.net/4fYuQyy-r8_rpBpcY7lH_A/falabellaCL/116978815_01/w=1500,h=1500,fit=pad",
+            },
+        ],
+    },
+    {
+        id: 2,
+        name: "Carnívoras",
+        image: "https://www.foodandwine.com/thmb/Wd4lBRZz3X_8qBr69UOu2m7I2iw=/1500x0/filters:no_upscale():max_bytes(150000):strip_icc()/classic-cheese-pizza-FT-RECIPE0422-31a2c938fc2546c9a07b7011658cfd05.jpg",
+        pizzas_disponibles: [
+            {
+                id: 1,
+                name: "Chorizo",
+                image: "https://i.pinimg.com/736x/1d/7e/a3/1d7ea361b00f35de9f2c4c2548daad97.jpg",
+                description: "Chorizo Parrillero",
+                price: 30,
+                bandera_image:
+                    "https://s1.significados.com/foto/bandera-de-argentina-og.jpg?class=ogImageWide",
+            },
+            {
+                id: 2,
+                name: "Salame",
+                image: "https://i.pinimg.com/736x/91/ed/cd/91edcda5ae837732353db81a0cf42341.jpg",
+                description: "Full Salame",
+                price: 28,
+                bandera_image:
+                    "https://imagedelivery.net/4fYuQyy-r8_rpBpcY7lH_A/falabellaCL/116978815_01/w=1500,h=1500,fit=pad",
+            },
+            {
+                id: 3,
+                name: "Tocino",
+                image: "https://i.pinimg.com/736x/ab/38/32/ab3832c7e6eed329ded5a7b28f7f530a.jpg",
+                description: "Jamón y Tocino Ahumado",
+                price: 28,
+                bandera_image:
+                    "https://st2.depositphotos.com/1000487/10460/v/950/depositphotos_104607398-stock-illustration-united-states-flag.jpg",
+            },
+        ],
+    },
+    {
+        id: 3,
+        name: "Tropicales",
+        image: "https://www.foodandwine.com/thmb/Wd4lBRZz3X_8qBr69UOu2m7I2iw=/1500x0/filters:no_upscale():max_bytes(150000):strip_icc()/classic-cheese-pizza-FT-RECIPE0422-31a2c938fc2546c9a07b7011658cfd05.jpg",
+        pizzas_disponibles: [
+            {
+                id: 1,
+                name: "Especiales",
+                image: "https://i.pinimg.com/736x/3d/98/8f/3d988f312f6877a8c72ba895dba44f74.jpg",
+                description: "Jamón, Hot Dog, Aceituna y Pimentón",
+                price: 32,
+                bandera_image:
+                    "https://imagedelivery.net/4fYuQyy-r8_rpBpcY7lH_A/falabellaCL/116885988_01/w=800,h=800,fit=pad",
+            },
+            {
+                id: 2,
+                name: "Aceituna",
+                image: "https://img.freepik.com/foto-gratis/rebanadas-pizza-aceitunas_23-2148357164.jpg?t=st=1738430658~exp=1738434258~hmac=eff6d09abcb4320340247349af437de1295bf7cd0e9861039583dbcea4574b48&w=740",
+                description: "Hot Dog Salchipapero y Aceituna",
+                price: 27,
+                bandera_image:
+                    "https://imagedelivery.net/4fYuQyy-r8_rpBpcY7lH_A/falabellaCL/116885988_01/w=800,h=800,fit=pad",
+            },
+            {
+                id: 3,
+                name: "Durazno",
+                image: "https://i.pinimg.com/736x/d7/f5/e4/d7f5e496c2c87ebdfdff8969c990d029.jpg",
+                description: "Jamón y Durazno en Almibar",
+                price: 32,
+                bandera_image:
+                    "https://media.istockphoto.com/id/530810572/es/vector/brasil-bandera-de-ilustraci%C3%B3n-vectorial.jpg?s=612x612&w=0&k=20&c=hu4BPeiDzfttyfE_NrNJuqoQmNIF04HTsaaV81_o88g=",
+            },
+        ],
+    },
+];
